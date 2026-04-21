4b5cbbf396a9fc42d4dc040a4f9589b233701f4f
kimminaaaa <tbrkcandy@naver.com>
2026-04-21 14:52:16 +0900

    d
    d

--- frontend/src/routers/router.tsx
+++ frontend/src/routers/router.tsx
@@ -12,11 +12,9 @@ import FAQ from "../pages/faq";
 import CustomerCenter from "../pages/CustomerService";
 import Login from "../layout/Login";
 import Brand from "../pages/Brand";
-<<<<<<< HEAD
 
-=======
 import Community from "../pages/Community";
->>>>>>> ed5380d82034649d9c50fa886e7c2d0941118365
+
 
 // (이론)성능 증가 팁 코딩: 코드 스플리팅(Route Code Splitting) 기능(페이지 뜨는 속도가 빨라짐)
 // 보충설명: 1) 기본: 화면이 뜨면 리액트의 모든 페이지를(부서,사원) 가져옵니다.
@@ -55,13 +53,9 @@ const router = createBrowserRouter([
       { path: "/login", element: <Login /> },
       // 브랜드 소개 페이지
       { path: "/brand", element: <Brand /> },
-<<<<<<< HEAD
-=======
+
       // 커뮤니티 소개 페이지
       { path: "/community", element: <Community /> }
-
->>>>>>> ed5380d82034649d9c50fa886e7c2d0941118365
-
 
 
       
