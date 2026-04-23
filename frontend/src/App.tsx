a65a4abe6a55911a09dd004af8e8af0dcdd24ae1
jungwoo031 <kjwproject256@gmail.com>
2026-04-23 09:17:06 +0900

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,11 @@
 import { HeadProvider } from "react-head";
 import { RouterProvider } from "react-router-dom";
-import "./App.css";
+import "./App.css"
 import router from "./routers/router";
 // 목적: 리액트가 실행하면 제일 처음 읽는 파일, 여기에 me 를 넣어도 됩니다.
 function App() {
 
   return (
-    // html 코딩
     <>
       {/* 메뉴 설정(RouterProvider), 검색엔진 순위 올림(HeadProvider) */}
       <HeadProvider>
