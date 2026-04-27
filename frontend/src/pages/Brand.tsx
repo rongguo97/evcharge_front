e129d0069a2672f5c8ea67c610dbeef816be63cd
gksalfus77-gif <gksalfus77@gmail.com>
2026-04-27 14:49:18 +0900

    수정
    수정

--- frontend/src/pages/Brand.tsx
+++ frontend/src/pages/Brand.tsx
@@ -1,11 +1,8 @@
 import React, { useEffect } from "react";
 import "../css/brand.css";
 import "../js/Brand.js";
-<<<<<<< HEAD
 import "../layout/Header.js";
-=======
 import "../layout/Footer.js";
->>>>>>> ef163990b753239ea3ab47db169e3c54e7010202
 import Footer from "../layout/Footer.js";
 
 const BrandPage: React.FC = () => {
